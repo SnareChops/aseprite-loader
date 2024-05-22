c9e4a7f6c304ca6729cc92c99d82fe7a6256e0db
SnareChops <snare_chops@yahoo.com>
2024-05-22 16:50:20 -0400

    Corrected return types of lib functions

--- lib/main.go
+++ lib/main.go
@@ -5,11 +5,11 @@ import (
 	"github.com/SnareChops/aseprite-loader/transform"
 )
 
-type FrameImage output.FrameImage
-type Frame output.Frame
-type Layer output.Layer
+type FrameImage = output.FrameImage
+type Frame = output.Frame
+type Layer = output.Layer
 
-func LoadFrames(path string) ([]output.FrameImage, error) {
+func LoadFrames(path string) ([]FrameImage, error) {
 	file, err := transform.File(path, "")
 	if err != nil {
 		return nil, err
@@ -17,7 +17,7 @@ func LoadFrames(path string) ([]output.FrameImage, error) {
 	return output.Frames(file)
 }
 
-func LoadSplitFramesAndLayers(path string) ([]output.Frame, error) {
+func LoadSplitFramesAndLayers(path string) ([]Frame, error) {
 	file, err := transform.File(path, "")
 	if err != nil {
 		return nil, err
