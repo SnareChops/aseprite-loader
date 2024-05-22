9b82a25ff7051b757cef51f5a97f2c212a9a6fc9
SnareChops <snare_chops@yahoo.com>
2024-05-22 17:29:47 -0400

    Added blend mode aliases to lib module

--- lib/main.go
+++ lib/main.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"github.com/SnareChops/aseprite-loader/internal"
 	"github.com/SnareChops/aseprite-loader/output"
 	"github.com/SnareChops/aseprite-loader/transform"
 )
@@ -8,6 +9,29 @@ import (
 type FrameImage = output.FrameImage
 type Frame = output.Frame
 type Layer = output.Layer
+type BlendMode = internal.BlendMode
+
+const (
+	BlendModeNormal     = internal.BlendModeNormal
+	BlendModeMultiply   = internal.BlendModeMultiply
+	BlendModeScreen     = internal.BlendModeScreen
+	BlendModeOverlay    = internal.BlendModeOverlay
+	BlendModeDarken     = internal.BlendModeDarken
+	BlendModeLighten    = internal.BlendModeLighten
+	BlendModeColorDodge = internal.BlendModeColorDodge
+	BlendModeColorBurn  = internal.BlendModeColorBurn
+	BlendModeHardLight  = internal.BlendModeHardLight
+	BlendModeSoftLight  = internal.BlendModeSoftLight
+	BlendModeDifference = internal.BlendModeDifference
+	BlendModeExclusion  = internal.BlendModeExclusion
+	BlendModeHue        = internal.BlendModeHue
+	BlendModeSaturation = internal.BlendModeSaturation
+	BlendModeColor      = internal.BlendModeColor
+	BlendModeLuminosity = internal.BlendModeLuminosity
+	BlendModeAddition   = internal.BlendModeAddition
+	BlendModeSubtract   = internal.BlendModeSubtract
+	BlendModeDivide     = internal.BlendModeDivide
+)
 
 func LoadFrames(path string) ([]FrameImage, error) {
 	file, err := transform.File(path, "")
